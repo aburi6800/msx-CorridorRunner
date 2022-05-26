2fcdf52111aaba04a19c37770ffc7f03736c5bee
aburi6800 <aburi6800@gmail.com>
2022-05-27 00:13:46 +0900

    Bug：5面以降、アイテムが表示されなくなる Fixes #51

--- src/msx/assets/pcgptn.asm
+++ src/msx/assets/pcgptn.asm
@@ -13,55 +13,55 @@ PCG_PTN_DATA:
 	DB $66, $f0, $f0, $f9, $fc, $ff, $9f, $df, $ff
 	DB $67, $4f, $af, $5f, $3f, $ff, $f9, $fb, $ff
 	DB $68, $00, $ab, $92, $ee, $a2, $b3, $20, $20
-	DB $69, $ff, $80, $9f, $80, $80, $ba, $a2, $b9
-	DB $6A, $ff, $01, $f9, $01, $01, $af, $a5, $25
-	DB $6B, $a2, $ba, $80, $87, $80, $9f, $80, $ff
-	DB $6C, $a5, $a5, $01, $e1, $01, $f9, $01, $ff
+	DB $69, $ff, $80, $bf, $b0, $80, $ba, $a2, $b9
+	DB $6A, $ff, $01, $fd, $0d, $01, $af, $a5, $25
+	DB $6B, $a2, $ba, $80, $b0, $b0, $bf, $80, $ff
+	DB $6C, $a5, $a5, $01, $0d, $0d, $fd, $01, $ff
 
     ; MAPCHIP 2
-	DB $70, $7f, $ff, $f4, $ff, $f7, $f7, $f7, $f7
-	DB $71, $fe, $ff, $2f, $ff, $ef, $ef, $ef, $ef
-	DB $72, $f7, $f7, $f7, $f7, $ff, $f4, $ff, $7f
-	DB $73, $ef, $ef, $ef, $ef, $ff, $2f, $ff, $fe
-	DB $74, $7f, $ff, $f4, $ff, $f7, $f7, $f7, $f7
-	DB $75, $fe, $ff, $2f, $ff, $ef, $ef, $ef, $ef
-	DB $76, $f7, $f7, $f7, $f7, $ff, $f4, $ff, $7f
-	DB $77, $ef, $ef, $ef, $ef, $ff, $2f, $ff, $fe
+	DB $70, $7d, $fd, $fc, $ff, $f7, $f7, $f7, $f7
+	DB $71, $be, $bf, $3f, $ff, $ef, $ef, $ef, $ef
+	DB $72, $f7, $f7, $f7, $f7, $ff, $fc, $fd, $7d
+	DB $73, $ef, $ef, $ef, $ef, $ff, $3f, $bf, $be
+	DB $74, $7d, $fd, $fc, $ff, $fc, $f8, $f0, $f0
+	DB $75, $be, $bf, $3f, $ff, $3f, $1f, $0f, $2f
+	DB $76, $f0, $f0, $f9, $fc, $ff, $fc, $fd, $7d
+	DB $77, $4f, $af, $5f, $3f, $ff, $3f, $bf, $be
 	DB $78, $00, $89, $d3, $4a, $10, $4a, $10, $00
-	DB $79, $ff, $80, $9f, $80, $80, $ba, $a2, $b9
-	DB $7A, $ff, $01, $f9, $01, $01, $af, $a5, $25
-	DB $7B, $a2, $ba, $80, $87, $80, $9f, $80, $ff
-	DB $7C, $a5, $a5, $01, $e1, $01, $f9, $01, $ff
+	DB $79, $ff, $80, $bf, $b0, $80, $ba, $a2, $b9
+	DB $7A, $ff, $01, $fd, $0d, $01, $af, $a5, $25
+	DB $7B, $a2, $ba, $80, $b0, $b0, $bf, $80, $ff
+	DB $7C, $a5, $a5, $01, $0d, $0d, $fd, $01, $ff
 
     ; MAPCHIP 3
-	DB $80, $f7, $f7, $f7, $17, $e8, $ef, $ef, $ef
-	DB $81, $ef, $ef, $ef, $e8, $17, $f7, $f7, $f7
-	DB $82, $ef, $ef, $ef, $e8, $17, $f7, $f7, $f7
-	DB $83, $f7, $f7, $f7, $17, $e8, $ef, $ef, $ef
-	DB $84, $f7, $f7, $f7, $17, $e8, $ef, $ef, $ef
-	DB $85, $ef, $ef, $ef, $e8, $17, $f7, $f7, $f7
-	DB $86, $ef, $ef, $ef, $e8, $17, $f7, $f7, $f7
-	DB $87, $f7, $f7, $f7, $17, $e8, $ef, $ef, $ef
-	DB $88, $00, $ee, $bb, $55, $aa, $00, $00, $00
-	DB $89, $ff, $80, $9f, $80, $80, $ba, $a2, $b9
-	DB $8A, $ff, $01, $f9, $01, $01, $af, $a5, $25
-	DB $8B, $a2, $ba, $80, $87, $80, $9f, $80, $ff
-	DB $8C, $a5, $a5, $01, $e1, $01, $f9, $01, $ff
+	DB $80, $f7, $ff, $ff, $57, $ea, $ff, $ff, $ef
+	DB $81, $ef, $ef, $ff, $ea, $57, $ff, $f7, $f7
+	DB $82, $ef, $ef, $ef, $fa, $57, $ff, $f7, $f7
+	DB $83, $f7, $ff, $ff, $57, $ea, $ff, $ff, $ef
+	DB $84, $f7, $ff, $ff, $57, $ec, $f8, $f0, $f0
+	DB $85, $ef, $ef, $ff, $ea, $37, $17, $0f, $2f
+	DB $86, $f0, $f0, $e9, $ec, $57, $ff, $f7, $f7
+	DB $87, $4f, $af, $5f, $37, $ea, $ff, $ff, $ef
+	DB $88, $09, $57, $ab, $56, $a2, $44, $00, $20
+	DB $89, $ff, $80, $bf, $b0, $80, $ba, $a2, $b9
+	DB $8A, $ff, $01, $fd, $0d, $01, $af, $a5, $25
+	DB $8B, $a2, $ba, $80, $b0, $b0, $bf, $80, $ff
+	DB $8C, $a5, $a5, $01, $0d, $0d, $fd, $01, $ff
 
     ; MAPCHIP 4
-	DB $90, $ff, $ff, $ff, $ff, $f7, $fe, $ff, $fb
-	DB $91, $ff, $ff, $ff, $ff, $ef, $7f, $ff, $df
-	DB $92, $fb, $ff, $fe, $f7, $ff, $ff, $ff, $ff
-	DB $93, $df, $ff, $7f, $ef, $ff, $ff, $ff, $ff
-	DB $94, $ff, $ff, $ff, $ff, $f7, $fe, $ff, $fb
-	DB $95, $ff, $ff, $ff, $ff, $ef, $7f, $ff, $df
-	DB $96, $fb, $ff, $fe, $f7, $ff, $ff, $ff, $ff
-	DB $97, $df, $ff, $7f, $ef, $ff, $ff, $ff, $ff
+	DB $90, $ff, $ff, $ff, $ef, $ff, $fe, $ff, $fb
+	DB $91, $ff, $ff, $ff, $f7, $ff, $7f, $ff, $df
+	DB $92, $fb, $ff, $fe, $ff, $ef, $ff, $ff, $ff
+	DB $93, $df, $ff, $7f, $ff, $f7, $ff, $ff, $ff
+	DB $94, $ff, $ff, $ff, $ef, $fc, $f8, $f0, $f0
+	DB $95, $ff, $ff, $ff, $f7, $3f, $1f, $0f, $2f
+	DB $96, $f0, $f0, $f9, $fc, $ef, $ff, $ff, $ff
+	DB $97, $5f, $af, $5f, $3f, $f7, $ff, $ff, $ff
 	DB $98, $00, $00, $c3, $00, $18, $00, $42, $00
-	DB $99, $ff, $80, $9f, $80, $80, $ba, $a2, $b9
-	DB $9A, $ff, $01, $f9, $01, $01, $af, $a5, $25
-	DB $9B, $a2, $ba, $80, $87, $80, $9f, $80, $ff
-	DB $9C, $a5, $a5, $01, $e1, $01, $f9, $01, $ff
+	DB $99, $ff, $80, $bf, $b0, $80, $ba, $a2, $b9
+	DB $9A, $ff, $01, $fd, $0d, $01, $af, $a5, $25
+	DB $9B, $a2, $ba, $80, $b0, $b0, $bf, $80, $ff
+	DB $9C, $a5, $a5, $01, $0d, $0d, $fd, $01, $ff
 
     ; POWER GUAGE
 	DB $AF, $00, $fe, $fe, $fe, $fe, $fe, $fe, $00
@@ -89,7 +89,7 @@ PCG_PTN_DATA:
 	DB $AB, $ff, $00, $18, $3c, $3c, $18, $00, $00
 	DB $AC, $0e, $0e, $0e, $0e, $0e, $06, $07, $03
 
-    DB 0
+    DB $00
 
 
 ; ■PCGカラーデータ
@@ -98,7 +98,7 @@ PCG_COLOR_DATA:
     DB $00, $00, $00, $00
     DB $F1, $F1, $F1, $F1
     DB $F1, $F1, $F1, $F1
-    DB $41, $41, $D1, $D1
+    DB $41, $41, $A1, $A1
     DB $C1, $C1, $1E, $1E
     DB $31, $31, $91, $71
     DB $F1, $F1, $F1, $F1
